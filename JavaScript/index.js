ab7f9dfe0425acf5c095a07ae2144353d3b9a95c
prabesh <prabeshdace@gmail.com>
2023-10-12 08:56:53 +0545

    Styling navbar

---
+++ JavaScript/index.js
@@ -0,0 +1,43 @@
+/**
+ * Hamburger menu for responsiveness
+ */
+var burgerMenu = document.getElementById('burger-menu');
+var overlay = document.getElementById('menu');
+burgerMenu.addEventListener('click',function(){
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+// Open the full screen search box
+function openSearch() {
+  document.getElementById("searchOverlay").style.display = "block";
+}
+
+// Close the full screen search box
+function closeSearch() {
+  document.getElementById("searchOverlay").style.display = "none";
+}
+
+
